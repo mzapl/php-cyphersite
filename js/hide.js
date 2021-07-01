6ceb0beadbe730ee361dbfea3bb5a41ed74dab85
s20681 <maciekz16@gmail.com>
2021-07-01 23:33:16 +0200

    moved optionally displayed inputs into main form. HTML now passes input values to the main php file.

--- js/hide.js
+++ js/hide.js
@@ -2,19 +2,22 @@ function resetsnippet(){
     $('.cipher').on('change', function() {
         var cipher = $('.cipher').val();
         console.warn(this.value)
-        if(cipher == "affine"){
-            $(".optional-affine").css('display', 'block')
-            $(".optional-vingenere").css('display', 'none')
+        if(cipher === "affine"){
+            $('input[name="optional-a"]').css('display', 'block')
+            $('input[name="optional-b"]').css('display', 'block')
+            $('input[name="optional-key"]').css('display', 'none')
         }
 
-        else if (cipher == "vingenere"){
-            $(".optional-affine").css('display', 'none')
-            $(".optional-vingenere").css('display', 'block')
+        else if (cipher === "vingenere"){
+            $('input[name="optional-a"]').css('display', 'none')
+            $('input[name="optional-b"]').css('display', 'none')
+            $('input[name="optional-key"]').css('display', 'block')
         }
 
         else{
-            $(".optional-affine").css('display', 'none')
-            $(".optional-vingenere").css('display', 'none')
+            $('input[name="optional-a"]').css('display', 'none')
+            $('input[name="optional-b"]').css('display', 'none')
+            $('input[name="optional-key"]').css('display', 'none')
         }
 
     });
--- main.php
+++ main.php
@@ -1,4 +1,21 @@
 <?php
-header('Content-type: text/plain');
-$ciphertext = $_GET['usertext'];
-echo "123455";
+function debug(){
+//    phpinfo();
+    var_dump($_POST);
+    echo "\n";
+
+    $func = $_GET;
+    if(count($_GET) == 0)
+        $func = $_POST;
+
+
+    $keys = array_keys($func);
+    foreach ($keys as $key){
+        echo "\['$key'] == {$func[$key]}<br/>";
+    }
+    echo "<br/>";
+    echo $_SERVER['REQUEST_METHOD'] . "<br/>";
+    echo $_SERVER['REQUEST_URI'];
+}
+
+debug();
